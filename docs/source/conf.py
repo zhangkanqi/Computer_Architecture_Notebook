5ba383cccc8d877160bb5ef72b44bda3e81e0e32
zhangkanqi <1308989543@qq.com>
2022-03-01 14:14:47 +0800

    修改template测试

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Computer_Architecture_Notebook'
+copyright = '2022, kkqq'
+author = 'kkqq'
 
-release = '0.1'
-version = '0.1.0'
+release = '1.0'
+version = '1.0.0'
 
 # -- General configuration
 
